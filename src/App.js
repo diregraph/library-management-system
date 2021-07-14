fa1edaf0214dd7b67f1a8d66a60de4faa83d7e12
Isuru Dharmadasa <isurud.14@cse.mrt.ac.lk>
2021-07-14 10:27:59 +0530

    Test styledComponents

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+`;
+
 function App() {
-    return <h1 style={{ textAlign: "center" }}>It's gonna be awesome!</h1>;
+    return (
+        <Wrapper>
+            <Title>It's gonna be awesome!</Title>
+        </Wrapper>
+    );
 }
 
 export default App;
