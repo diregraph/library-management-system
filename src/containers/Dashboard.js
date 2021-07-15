f3465f51cb100ffe0eb69b61264306c0ec80374b
Isuru Dharmadasa <isurud.14@cse.mrt.ac.lk>
2021-07-15 09:58:48 +0530

    Refactor Tabs component to have dynamic content

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,36 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
-import { Tabs, Tab,TabContent, Content } from "../components/Tabs";
+import Tabs from "../components/Tabs";
 
-const Dashboard = (props) => {
-    const [active, setActive] = useState(0);
-    const handleClick = (e) => {
-        const index = parseInt(e.target.id, 0);
-        if (index !== active) {
-            setActive(index);
-        }
-    };
+const Dashboard = () => {
+    const contents = [
+        { title: "Books", elements: <h1>Contents of books go here</h1> },
+        { title: "Members", elements: <h1>Contents of members go here</h1> },
+    ];
 
-    return (
-        <>
-            <Tabs>
-                <Tab onClick={handleClick} active={active === 0} id={0}>
-                    Content1
-                </Tab>
-                <Tab onClick={handleClick} active={active === 1} id={1}>
-                    Content2
-                </Tab>
-                <TabContent>
-                    <Content active={active === 0}>
-                        <h1>Content 1</h1>
-                    </Content>
-                    <Content active={active === 1}>
-                        <h1>Content 2</h1>
-                    </Content>
-                </TabContent>
-            </Tabs>
-        </>
-    );
+    return <Tabs contents={contents} />;
 };
 
 export default Dashboard;
