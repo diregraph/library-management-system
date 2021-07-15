8973ece3004b31526192b52a041566819db1e1b0
Isuru Dharmadasa <isurud.14@cse.mrt.ac.lk>
2021-07-15 07:48:40 +0530

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { Header, Main, Footer } from "./components/Layout";
+import { NavBar, NavItem, NavLink } from "./components/Navbar";
 
 function App() {
     return (
         <>
-            <Header>This is the header</Header>
-            <Main>
-                This is the main content
-            </Main>
+            <Header>
+                <NavBar>
+                    <NavItem>
+                        <NavLink href="default.asp">Catalog</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="news.asp">Dashboard</NavLink>
+                    </NavItem>
+                </NavBar>
+            </Header>
+            <Main>This is the main content</Main>
             <Footer>This is the footer</Footer>
         </>
     );
