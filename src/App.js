cb7bd6383f313fcb5419905533247671c6c375e2
Isuru Dharmadasa <isurud.14@cse.mrt.ac.lk>
2021-07-15 07:10:37 +0530

    Add main layout for the app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: palevioletred;
-`;
-
-// Create a Wrapper component that'll render a <section> tag with some styles
-const Wrapper = styled.section`
-    padding: 4em;
-    background: papayawhip;
-`;
+import { Header, Main, Footer } from "./components/Layout";
 
 function App() {
     return (
-        <Wrapper>
-            <Title>It's gonna be awesome!</Title>
-        </Wrapper>
+        <>
+            <Header>This is the header</Header>
+            <Main>
+                This is the main content
+            </Main>
+            <Footer>This is the footer</Footer>
+        </>
     );
 }
 
