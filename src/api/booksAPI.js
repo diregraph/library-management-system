c064e5a6b72614545f770497c8b57bea977db6c5
Isuru Dharmadasa <isurud.14@cse.mrt.ac.lk>
2021-07-16 08:52:09 +0530

    Add burrow API to booksAPI

--- src/api/booksAPI.js
+++ src/api/booksAPI.js
@@ -6,3 +6,6 @@ const BASE_URL = "/book";
 export const getBooks = () => getRequest(BASE_URL);
 
 export const getBook = (id) => getRequest(`${BASE_URL}/${id}`);
+
+export const lendBook = (id, burrowedMemberId, burrowedDate) =>
+    putRequest(`${BASE_URL}/${id}/burrow`, { burrowedMemberId, burrowedDate });
